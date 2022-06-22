425b2305b3912161f5f8425ac241c331890f1b73
Aska Erlangga <ae.askaerlangga@gmail.com>
2022-06-22 12:56:36 +0700

    add signup alert dialog

--- lib/app/modules/signup/controllers/signup_controller.dart
+++ lib/app/modules/signup/controllers/signup_controller.dart
@@ -7,21 +7,39 @@ class SignupController extends GetxController {
   TextEditingController email = TextEditingController();
   TextEditingController password = TextEditingController();
 
+  // Alert Dialog
+  void alertDialog(String message) {
+    Get.defaultDialog(
+        title: 'Perhatian', middleText: message, onConfirm: () => Get.back());
+  }
+
   // Fungsi SignUp
   void signup(String email, String password) async {
     try {
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
+      if (this.email.text != '' && this.password.text != '') {
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        Get.defaultDialog(
+            title: 'Daftar akun berhasil',
+            middleText: 'Sekarang kamu sudah bisa login',
+            onConfirm: () {
+              Get.back();
+              Get.back();
+            });
+      } else {
+        alertDialog('Email dan password tidak boleh kosong');
+      }
     } on FirebaseAuthException catch (e) {
       if (e.code == 'weak-password') {
-        // print('The password provided is too weak.');
-        Get.defaultDialog(middleText: 'The password provided is too weak.');
+        alertDialog(
+            'Password yang kamu masukkan terlalu lemah, dan pastikan terdiri dari 6 karakter');
       } else if (e.code == 'email-already-in-use') {
-        // print('The account already exists for that email.');
-        Get.defaultDialog(
-            middleText: 'The account already exists for that email.');
+        alertDialog(
+            'Email yang kamu masukkan sudah terdaftar.\nsilahkan gunakan email lain');
+      } else if (e.code == 'invalid-email') {
+        alertDialog('Penulisan email salah.\ncontoh : emailku@gmail.com');
       }
     } catch (e) {
       Get.defaultDialog(middleText: e.toString());
