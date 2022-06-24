aac6c7abbbd7558629a65c35f2ccbfc156f67d80
Aska Erlangga <ae.askaerlangga@gmail.com>
2022-06-24 16:46:35 +0700

    Memperbarui : Home pengendara

--- lib/app/modules/home/views/pengendara_view.dart
+++ lib/app/modules/home/views/pengendara_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:unbaja_parking_app/app/modules/home/controllers/home_controller.dart';
+import 'package:unbaja_parking_app/app/modules/home/views/widgets/menu_button.dart';
+import 'package:unbaja_parking_app/app/modules/home/views/widgets/user_info_panel.dart';
 
 class PengendaraView extends GetView<HomeController> {
   const PengendaraView({Key? key}) : super(key: key);
@@ -9,7 +11,7 @@ class PengendaraView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('PENGENDARA'),
+        title: const Text('UNBAJA Parking'),
         centerTitle: true,
         actions: [
           IconButton(
@@ -19,6 +21,34 @@ class PengendaraView extends GetView<HomeController> {
               icon: const Icon(Icons.logout))
         ],
       ),
+      body: ListView(children: <Widget>[
+        UserInfoPanel(
+          nameUser: controller.nameUser.value,
+          levelUser: controller.levelUser.value,
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Container(
+          padding: const EdgeInsets.fromLTRB(20, 10, 20, 20),
+          child: Column(
+            children: <Widget>[
+              MenuButton(
+                  label: 'Tampil QR Code',
+                  icon: Icons.qr_code,
+                  onPressed: () {}),
+              MenuButton(
+                  label: 'Kendaraan Saya',
+                  icon: Icons.directions_car,
+                  onPressed: () {}),
+              MenuButton(
+                  label: 'Pengaturan Profil',
+                  icon: Icons.person,
+                  onPressed: () {}),
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
