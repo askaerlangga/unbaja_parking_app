33cc47352da6c7f6c0fb01c08f1342e234067a51
Aska Erlangga <ae.askaerlangga@gmail.com>
2022-06-26 23:54:07 +0700

    Menambahkan : Form masukan manual QR Code scanner

--- lib/app/modules/qrcode_scanner/controllers/qrcode_scanner_controller.dart
+++ lib/app/modules/qrcode_scanner/controllers/qrcode_scanner_controller.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
 class QrcodeScannerController extends GetxController {
   MobileScannerController cameraController = MobileScannerController();
   var scanCode = ''.obs;
+  TextEditingController nomorPlat = TextEditingController();
 }
--- lib/app/modules/qrcode_scanner/views/qrcode_scanner_view.dart
+++ lib/app/modules/qrcode_scanner/views/qrcode_scanner_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:unbaja_parking_app/app/widgets/custom_button.dart';
 
 import '../controllers/qrcode_scanner_controller.dart';
 
@@ -14,68 +15,79 @@ class QrcodeScannerView extends GetView<QrcodeScannerController> {
         title: const Text('QR Code Scanner'),
         centerTitle: true,
       ),
-      body: Column(children: [
-        SizedBox(
-            height: Get.height * 0.55,
-            child: Stack(
-              children: <Widget>[
-                MobileScanner(
-                    controller: controller.cameraController,
-                    allowDuplicates: false,
-                    onDetect: (barcode, args) {
-                      if (barcode.rawValue == null) {
-                        debugPrint('Failed to scan Barcode');
-                      } else {
-                        final String code = barcode.rawValue!;
-                        controller.scanCode.value = code;
-                        debugPrint('Barcode found! $code');
-                      }
-                    }),
-                // Container(
-                //   width: 100,
-                //   height: 100,
-                //   decoration:
-                //       BoxDecoration(color: Color.fromARGB(20, 255, 255, 255)),
-                // ),
-                Align(
-                  alignment: const Alignment(0, 0.9),
-                  child: SizedBox(
-                      height: 60,
-                      width: 60,
-                      child: MaterialButton(
-                        onPressed: () {
-                          controller.cameraController.toggleTorch();
-                        },
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50)),
-                        color: Colors.blue,
-                        child: ValueListenableBuilder(
-                          valueListenable:
-                              controller.cameraController.torchState,
-                          builder: (context, state, child) {
-                            return ((state as TorchState) == TorchState.off)
-                                ? const Icon(
-                                    Icons.flashlight_off,
-                                    color: Colors.white,
-                                  )
-                                : const Icon(
-                                    Icons.flashlight_on,
-                                    color: Colors.white,
-                                  );
-                          },
-                        ),
-                      )),
-                )
-              ],
-            )),
-        Flexible(
-            child: SizedBox(
-                height: Get.height * 0.45,
-                child: Center(
-                  child: Obx(
-                    () => Text('${controller.scanCode.value}'),
+      body: Stack(alignment: Alignment.bottomCenter, children: [
+        MobileScanner(
+            controller: controller.cameraController,
+            allowDuplicates: false,
+            onDetect: (barcode, args) {
+              if (barcode.rawValue == null) {
+                debugPrint('Failed to scan Barcode');
+              } else {
+                final String code = barcode.rawValue!;
+                controller.scanCode.value = code;
+                debugPrint('Barcode found! $code');
+              }
+            }),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            SizedBox(
+                height: 60,
+                width: 60,
+                child: MaterialButton(
+                  onPressed: () {
+                    controller.cameraController.toggleTorch();
+                  },
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                  color: Colors.blue,
+                  child: ValueListenableBuilder(
+                    valueListenable: controller.cameraController.torchState,
+                    builder: (context, state, child) {
+                      return ((state as TorchState) == TorchState.off)
+                          ? const Icon(
+                              Icons.flashlight_off,
+                              color: Colors.white,
+                            )
+                          : const Icon(
+                              Icons.flashlight_on,
+                              color: Colors.white,
+                            );
+                    },
                   ),
-                ))),
+                )),
+            SizedBox(height: 20),
+            Flexible(
+                child: Container(
+              padding: EdgeInsets.all(20),
+              height: 220,
+              color: Colors.white,
+              child: Column(
+                children: [
+                  const Text(
+                    'Masukan Manual',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    controller: controller.nomorPlat,
+                    decoration: const InputDecoration(
+                        labelText: 'Nomor Plat Kendaraan',
+                        border: OutlineInputBorder()),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  CustomButton(label: 'PROSES', onPressed: () {})
+                ],
+              ),
+            ))
+          ],
+        )
       ]),
     );
   }
