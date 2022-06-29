9c1e46d421124a618f722daeed0f592519e98832
Aska Erlangga <ae.askaerlangga@gmail.com>
2022-06-29 16:10:09 +0700

    Memperbaiki : Tanda tidak ada kendaraan terparkir

--- lib/app/modules/petugas/kendaraan_terparkir/views/kendaraan_terparkir_view.dart
+++ lib/app/modules/petugas/kendaraan_terparkir/views/kendaraan_terparkir_view.dart
@@ -22,69 +22,70 @@ class KendaraanTerparkirView extends GetView<KendaraanTerparkirController> {
             if (snapshot.connectionState == ConnectionState.active &&
                 snapshot.data != null) {
               var listKendaraan = snapshot.data!.docs;
-              print(listKendaraan);
-              return ListView.builder(
-                  padding: EdgeInsets.all(20),
-                  itemCount: listKendaraan.length,
-                  itemBuilder: (context, index) {
-                    var dataKendaraan = listKendaraan[index].data();
-                    print('DATA KENDARAAN ${dataKendaraan}');
-                    return StreamBuilder<
-                            DocumentSnapshot<Map<String, dynamic>>>(
-                        stream: controller
-                            .getDataKendaraan(dataKendaraan['kendaraan']),
-                        builder: (context, snapshot) {
-                          if (snapshot.data != null &&
-                              snapshot.data?.data() != null) {
-                            var data = snapshot.data!.data()!;
-                            return GestureDetector(
-                              onTap: () => Get.toNamed(
-                                  Routes.DETAIL_KENDARAAN_TERPARKIR),
-                              child: Card(
-                                margin: const EdgeInsets.only(bottom: 10),
-                                child: SizedBox(
-                                  height: 80,
-                                  child: Row(children: [
-                                    Padding(
-                                      padding: const EdgeInsets.only(left: 10),
-                                      child: Column(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: <Widget>[
-                                          Text(
-                                            data['nomor_plat'],
-                                            overflow: TextOverflow.ellipsis,
-                                            maxLines: 1,
-                                            style: const TextStyle(
-                                                fontSize: 20,
-                                                fontWeight: FontWeight.bold),
-                                          ),
-                                          Text(
-                                            data['merek_kendaraan'],
-                                            overflow: TextOverflow.ellipsis,
-                                            maxLines: 1,
-                                          )
-                                        ],
+              print('LIST $listKendaraan');
+              if (listKendaraan.isNotEmpty) {
+                return ListView.builder(
+                    padding: EdgeInsets.all(20),
+                    itemCount: listKendaraan.length,
+                    itemBuilder: (context, index) {
+                      var dataKendaraan = listKendaraan[index].data();
+                      print('DATA KENDARAAN ${dataKendaraan}');
+                      return StreamBuilder<
+                              DocumentSnapshot<Map<String, dynamic>>>(
+                          stream: controller
+                              .getDataKendaraan(dataKendaraan['kendaraan']),
+                          builder: (context, snapshot) {
+                            if (snapshot.data != null &&
+                                snapshot.data?.data() != null) {
+                              var data = snapshot.data!.data()!;
+                              return GestureDetector(
+                                onTap: () => Get.toNamed(
+                                    Routes.DETAIL_KENDARAAN_TERPARKIR),
+                                child: Card(
+                                  margin: const EdgeInsets.only(bottom: 10),
+                                  child: SizedBox(
+                                    height: 80,
+                                    child: Row(children: [
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(left: 10),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: <Widget>[
+                                            Text(
+                                              data['nomor_plat'],
+                                              overflow: TextOverflow.ellipsis,
+                                              maxLines: 1,
+                                              style: const TextStyle(
+                                                  fontSize: 20,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            Text(
+                                              data['merek_kendaraan'],
+                                              overflow: TextOverflow.ellipsis,
+                                              maxLines: 1,
+                                            )
+                                          ],
+                                        ),
                                       ),
-                                    ),
-                                    const Spacer(
-                                      flex: 1,
-                                    ),
-                                  ]),
+                                      const Spacer(
+                                        flex: 1,
+                                      ),
+                                    ]),
+                                  ),
                                 ),
-                              ),
-                            );
-                          }
-                          return const Center(
-                              child: Text('Tidak ada kendaraan terparkir'));
-                        });
-                  });
+                              );
+                            }
+                            return Text('');
+                          });
+                    });
+              }
+              return const Center(child: Text('Tidak ada kendaraan terparkir'));
             }
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
+            return const Center(child: CircularProgressIndicator());
           },
         ));
   }
